0dd7cba9c7e27fc65eb0fc16be9b130250ba4d0e
Lugard Agu <agulugard@gmail.com>
2024-02-19 14:58:23 +0100

    Remove padding from App.js and Add it to Home.js file

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -6,7 +6,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function Home() {
   return (
     <SafeAreaView>
-      <View>
+      <View style={{padding: 20}}>
         <Header />
       </View>
     </SafeAreaView>
