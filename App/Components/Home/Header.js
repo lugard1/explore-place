fc45d82642869754f73e0728c3891c670eb78471
Lugard Agu <agulugard@gmail.com>
2024-02-19 14:55:20 +0100

    Add padding to App.js and style logo icon

--- App/Components/Home/Header.js
+++ App/Components/Home/Header.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
+// import { Image } from 'react-native'
 
 export default function Header() {
   return (
     <View>
-      <Text>Header Component</Text>
+      <Image source={require('./../../../assets/logo.png')}
+      style={styles.logo} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  logo:{
+    width: 50,
+    height: 50
+  }
+})
--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import Header from '../Components/Home/Header';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
   return (
-    <View>
-      <Header />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Header />
+      </View>
+    </SafeAreaView>
   )
 }
