00fe6ed6c87570f294310a6016da3b985de91a8b
Lugard Agu <agulugard@gmail.com>
2024-02-19 18:14:27 +0100

    Added Google Map View File and import google maps

---
+++ App/Components/Home/GoogleMapView.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+import React from 'react';
+
+export default function GoogleMapView() {
+  return (
+    <View>
+      <Text>GoogleMapView</Text>
+    </View>
+  )
+}
