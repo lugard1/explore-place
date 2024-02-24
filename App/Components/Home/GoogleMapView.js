810231e537e6b1a2605480be855a6eaad1ac8734
Lugard Agu <agulugard@gmail.com>
2024-02-24 12:50:57 +0100

    Add expo location

--- App/Components/Home/GoogleMapView.js
+++ App/Components/Home/GoogleMapView.js
@@ -1,14 +1,21 @@
 import { View, Text, Dimensions } from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, mapRegion} from 'react-native-maps';
 import React from 'react';
 
 export default function GoogleMapView() {
-  const handleMapError = (error) => {
-    console.error('MapView Error:', error);
-  };
+  // const handleMapError = (error) => {
+  //   console.error('MapView Error:', error);
+  // };
+
+  const [mapRegion, setMapRegion] = UseState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0522,
+    longitudeDelta: 0.0421
+  })
 
   return (
-    <View style={{marginTop:20}}>
+    <View style={{marginTop:20, borderRadius: 8}}>
       <MapView
       style={{
         width:Dimensions.get('screen').width*0.89,
@@ -17,7 +24,8 @@ export default function GoogleMapView() {
       }}
       provider={PROVIDER_GOOGLE}
       showsUserLocation={true}
-      onError={handleMapError} // Add error handling
+      region={mapRegion}
+      // onError={handleMapError} // Add error handling
       >
 
       </MapView>
