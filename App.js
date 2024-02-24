781c4479f7cd70431ed94bb4272a727e9eb178f1
Lugard Agu <agulugard@gmail.com>
2024-02-24 12:54:44 +0100

    Add useEffect for Location in App.js file

--- App.js
+++ App.js
@@ -4,6 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import TabNavigation from './App/Navigations/TabNavigation';
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+  
   return (
     <View style={styles.container}>
       <NavigationContainer>
