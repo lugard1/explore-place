7fb673f463001610b388759266add6b5e820e84d
Lugard Agu <agulugard@gmail.com>
2024-02-24 15:09:44 +0100

    Add google fonts

--- App/Components/Home/GoogleMapView.js
+++ App/Components/Home/GoogleMapView.js
@@ -1,6 +1,6 @@
 import { View, Text, Dimensions } from 'react-native';
 import MapView, {PROVIDER_GOOGLE, mapRegion} from 'react-native-maps';
-import React, { useEffect, useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { UserLocationContext } from '../../Context/UserLocationContext';
 
 export default function GoogleMapView() {
@@ -8,28 +8,41 @@ export default function GoogleMapView() {
   //   console.error('MapView Error:', error);
   // };
 
-  const [mapRegion, setMapRegion] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0522,
-    longitudeDelta: 0.0421
-  })
-  const {location, setLocation} = useContext(UserLocationContext)
+  const [mapRegion, setMapRegion] = useState([])
+  const {location, setLocation} = useContext(UserLocationContext);
+  useEffect(()=>{
+    if(location)
+    {
+      setMapRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0522,
+        longitudeDelta: 0.0421
+      })
+    }
+  },[])
+
+
   return (
-    <View style={{marginTop:20}}>
+    <View style={{ marginTop: 20 }}>
+      <Text style={{fontSize: 20, marginBottom: 10, fontWeight: 600}}>
+        Top Near By Places
+      </Text>
+      <View style={{borderRadius: 20, overflow: 'hidden'}}>
       <MapView
-      style={{
-        width:Dimensions.get('screen').width*0.89,
-        height:Dimensions.get('screen').height*0.23,
-        borderRadius:20
-      }}
-      provider={PROVIDER_GOOGLE}
-      showsUserLocation={true}
-      region={mapRegion}
-      // onError={handleMapError} // Add error handling
-      >
+        style={{
+          width: Dimensions.get('screen').width * 0.89,
+          height: Dimensions.get('screen').height * 0.23,
+          
+        }}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={true}
+          region={mapRegion}
+        >
 
       </MapView>
+      </View>
+     
     </View>
   )
 }
